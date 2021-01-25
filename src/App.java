4c14a7f8dc2776e06f77575adc0ca132e4c1630a
Mohammed Thaabit Jacobs <mohammedthaabitjacobs@gmail.com>
2021-01-25 20:44:23 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) {
+        List<String> name = new ArrayList<>();
+        name.add("Thaabit");
+        name.add("Kauthar");
+
+        System.out.println(name);
+    }
+}
